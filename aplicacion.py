30776f77d34809c431a6f75751c5419482dba4f3
raulandresvesgneira <raulandresvesgneira@gmail.com>
2024-07-12 15:21:37 +0000

    v2

---
+++ aplicacion.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.html("<center><h1>Bienvenido al codificador de texto tipo César</h1></center>")
+st.markdown("---")
+st.markdown("El cifrado César, también conocido como cifrado por desplazamiento, es un tipo de cifrado por sustitución. En él, cada letra en el texto original se reemplaza por otra letra que se encuentra un número fijo de posiciones más adelante en el alfabeto.")
+st.markdown("---")
+st.image("https://ayudaleyprotecciondatos.es/wp-content/uploads/2020/06/cifrado-cesar-ROT-13.png")
+st.markdown("---")
